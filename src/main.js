62e71d4afac47c081e76d1341c4fab0a1c1e5427
f4Lq <167790412+f4Lq@users.noreply.github.com>
2026-04-30 18:17:47 +0200

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+window.React = React;
+window.ReactDOM = { createRoot };
+
+const appVersion = "20260430-react-1";
+const existingScript = document.querySelector('script[data-app-loader="legacy-react"]');
+
+if (!existingScript) {
+  const script = document.createElement("script");
+  script.src = `/js/app.js?v=${encodeURIComponent(appVersion)}`;
+  script.defer = true;
+  script.dataset.appLoader = "legacy-react";
+  document.head.appendChild(script);
+}
